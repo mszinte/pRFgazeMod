3e7f861276109452bcc83bb53eb4a8846e594a5f
Knapen Lab <tknapen@gmail.com>
2018-12-17 13:31:12 +0100

    linux machine test and stimuli processing

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -25,10 +25,10 @@ Screen('CloseAll');clear all;clear mex;clear functions;close all;home;ListenChar
 % General settings
 % ----------------
 const.expName           =   'pRFgazeMod';   % experiment name.
-const.expStart          =   0;              % Start of a recording exp                          0 = NO  , 1 = YES
+const.expStart          =   1;              % Start of a recording exp                          0 = NO  , 1 = YES
 const.checkTrial        =   0;              % Print trial conditions (for debugging)            0 = NO  , 1 = YES
 const.writeLogTxt       =   1;              % write a log file in addition to eyelink file      0 = NO  , 1 = YES
-const.genStimuli        =   1;              % Generate all stimuli                              0 = NO  , 1 = YES
+const.genStimuli        =   0;              % Generate all stimuli                              0 = NO  , 1 = YES
 const.drawStimuli       =   0;              % Draw stimuli generated                            0 = NO  , 1 = YES
 const.mkVideo           =   0;              % Make a video of a run (on mac not linux)          0 = NO  , 1 = YES
 
